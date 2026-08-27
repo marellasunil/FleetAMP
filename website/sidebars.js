65e6319ff585a034a066ae99d3fd4292ef0451a5
Sunil Marella <marella.sunil@outlook.com>
2026-08-27 14:35:22 +0200

    docs: add documentation navigation

---
+++ website/sidebars.js
@@ -0,0 +1,30 @@
+module.exports = {
+  docsSidebar: [
+    {
+      type: 'category',
+      label: 'Getting Started',
+      items: ['getting-started/overview', 'getting-started/quick-start'],
+    },
+    {
+      type: 'category',
+      label: 'Concepts',
+      items: ['concepts/architecture', 'concepts/managed-agents', 'concepts/attributes-and-labels'],
+    },
+    {
+      type: 'category',
+      label: 'Fleet Management',
+      items: ['fleet-management/inventory', 'fleet-management/configuration'],
+    },
+    {
+      type: 'category',
+      label: 'Integrations',
+      items: ['integrations/overview'],
+    },
+    {
+      type: 'category',
+      label: 'Development',
+      items: ['development/build-from-source'],
+    },
+    'roadmap',
+  ],
+};
